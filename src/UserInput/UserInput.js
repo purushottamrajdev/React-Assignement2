cdfbda7f6023d5c3d41b1116e9a56ce6476ce479
sachin.rajdev <sachin281996@gmail.com>
2020-02-23 22:55:03 +0530

    Testing for pull request

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -5,6 +5,7 @@ const UserInput=(props)=>
      return(
          <div className="UserInput">
          <h1>User Input</h1>
+          <h1>Test git repo</h1>
          <input className="InputText" type="text" onChange={props.changed} value={props.value} />
          </div>
      )
